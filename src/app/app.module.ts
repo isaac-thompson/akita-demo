81005769aa23fc515bba45eb6a58079d357ede07
isaac-thompson <isaac.thompson@canada.ca>
2021-03-17 15:46:48 -0300

    I am following demo example of using akita for state management in Angular. - initialize akita demo project - added 2 components: home and add-todo - cleared content of app.component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AddTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
