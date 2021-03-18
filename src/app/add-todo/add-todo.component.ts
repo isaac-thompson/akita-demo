2c41194f96afdf901dfa79a0c0a182d25bd448fa
isaac-thompson <isaac.thompson@canada.ca>
2021-03-18 15:33:53 -0300

    State Management in Angular using Akita, Part 7 - UI and Forms. https://www.youtube.com/watch?v=2Re3Fm8fV8o&list=PLaY6YJMqp51eoN-bVk01nwEmYJ2cB7UUz&index=7 using latest version of TS with strictPropertyInitialization enabled... causes problems... initially geting error "property form has no initializer and is not definitely assigned in the constructor" check out this link for instructions to overcome the problem - https://fireflysemantics.medium.com/initializing-your-angular-reactive-form-1c2f4c09fdb2

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({
+    title: new FormControl(null, [Validators.required]),
+    description: new FormControl(null, [Validators.required])
+  });
+
+  constructor() {
+  }
+
+  initializeForm(): void {
+    this.form.setValue({
+      title: '',
+      description: ''
+    });
+  }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  addTodo(): void {
+    console.log(this.form.value);
   }
 
 }
